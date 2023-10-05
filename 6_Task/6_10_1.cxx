89acbfbb20baf00067a0bd1fcc1c896f107c605c
Mr-Wolfo <109300858+Mr-Wolfo@users.noreply.github.com>
2023-10-05 10:45:40 +0200

    Исправление недочёта

--- 6_Task/6_10_1.cxx
+++ 6_Task/6_10_1.cxx
@@ -5,16 +5,23 @@ int main()
 	
 	int j; //кол-во чисел в последовательности
 	int sum = 0;
-	int min = 9999999;
+	int min;
 	int i; //номер мин. числа ≠ ...1
 	int x;
 	int count = 1; //номер числа в последовательности
+	bool flag = true;
 	
 	std::cin >> j;
 	
+
 	while (j)
 	{
 		std::cin >> x;
+		
+		if (flag == true) {
+			min = x;
+			flag = false;
+		}
 		
 		if (x % 10 != 1) {
 			sum += x;
