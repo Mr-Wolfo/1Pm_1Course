98457b0af72a8f9b653926b7f4a2130b4f96e746
Mr-Wolfo <maximus2690@gmail.com>
2024-02-02 21:51:46 +0300

    Lab 2

---
+++ 2_Sem/3_Task/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+class String {
+    char *str;
+    int m_len;
+
+public:
+    // Конструктор
+    String(int len) {
+        m_len = len;
+        str = new char[len];
+    }
+
+    // Конструктор копирования
+    String(const String &other) : String(other.m_len) {
+        for (int i = 0; i < m_len; i++)
+            str[i] = other.str[i];
+    }
+
+    // Оператор присваивания
+    String &operator=(const String &other) {
+        delete[] str;
+        m_len = other.m_len;
+
+        str = new char[m_len];
+        for (int i = 0; i < m_len; i++)
+            str[i] = other.str[i];
+
+        return *this;
+    }
+
+    void StrOut() {
+        std::cout << m_len << std::endl;
+    }
+
+    // Деконструктор
+    ~String() {
+        delete[] str;
+    }
+};
+
+int main ()
+{
+    String A(5);
+    A.StrOut();
+    String B(10);
+    B.StrOut();
+
+    B = A;
+    B.StrOut();
+
+    String C(B);
+    C.StrOut();
+
+    return 1;
+}
